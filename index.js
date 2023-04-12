d38a6248b8fa0c0df5af985e2da3a7ffcd421085
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-12 11:36:14 +0300

    added a DOMcontent loaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+//event listener for the domcontent loaded
+document.addEventListener("DOMContentLoaded", function(){
+    //accessnig the element
+     const text = document.getElementById("text");
+     text.textContent = "THIs is Cool"
+})
